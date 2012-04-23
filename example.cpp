11575b6054b7d7568ccbf1fe10ab3cfebc49afd3
Stanislav Korotaev <superstals@gmail.com>
2012-04-23 17:13:36 +0400

    rewrote example.cpp with new titleMacher functionality

--- example.cpp
+++ example.cpp
@@ -6,6 +6,9 @@
 
 #include "titlematcher.h"
 
+TitleMatcher titleMatcher;
+
+
 void printTitle(Title title){
 	std::cout<<title.name<<std::endl;
 	std::cout<<title.date<<std::endl;
@@ -13,40 +16,55 @@ void printTitle(Title title){
 	std::cout<<title.description<<std::endl;
 }
 
-int main(){
-	TitleMatcher titleMatcher;
+void printTitles(){
+	//TODO print 1 and 2 for title so that user can understand
+	std::cout<<"\n------------------------------------"<<std::endl;
+	printTitle(titleMatcher.title_1);
+	std::cout<<"\n-----------------VS-----------------"<<std::endl;
+	printTitle(titleMatcher.title_2);
+	std::cout<<"\n------------------------------------"<<std::endl;
+}
+
+void printTop5(){
+	const std::list<int> lst = titleMatcher.getTitles(5);
+	int i = 1;
+	std::cout<<"Top 5 games:"<<std::endl;
+	for (std::list<int>::const_iterator it = lst.begin(); it!= lst.end(); ++it, ++i){
+		std::cout<<i<<". "<<(*it)<<std::endl;
+	}
+	std::cout<<std::endl;
+}
+
+void match(){
 	while(true){
-		//TODO print 1 and 2 for title so that user can understand
-		printTitle(titleMatcher.title_1);
-		std::cout<<"-----------------VS-----------------"<<std::endl;
-		printTitle(titleMatcher.title_2);
+		std::string input;
 
-		
-		//TODO rewrite this part i guess.
-		std::string userChoice;
-		std::cout<<"What will be your choice? (1/2):"; //TODO tell i need to get 1 or 2
-		std::cin>>userChoice;
-		
-		int choice;
-		//magickly turn it to int
-		if (userChoice == "1")
-			choice = 1;
-		else if(userChoice == "2")
-			choice = 2; 
-		else{
-			std::cout<<"You can only print 1 or 2"<<std::endl;
-			choice = 2; //TODO delete - just for now to check if everything is working
-		}	
+		printTitles();
+		std::cout<<"Print '1' or '2' to select that title.\nOr print '-1' and '-2' if you dont know it.\nPrint 'quit' to exit: ";
+		std::cin>>input;
+		if(input == "1")
+			titleMatcher.submitChoice(1);
+		else if(input == "2")
+			titleMatcher.submitChoice(2);
+		else if(input == "-1")
+			titleMatcher.dontKnowTitle(1);
+		else if(input == "-2")
+			titleMatcher.dontKnowTitle(2);
+		else if(input == "quit")
+			break;
+	}
+}
 
-		titleMatcher.submitChoice(choice);
 
-		const std::list<int> lst = titleMatcher.getTitles(5);
-		int i = 1;
-		std::cout<<"Top 5 games:"<<std::endl;
-		for (std::list<int>::const_iterator it = lst.begin(); it!= lst.end(); ++it, ++i){
-			std::cout<<i<<". "<<(*it)<<std::endl;
-		}
-		std::cout<<std::endl;
+int main(){
+	std::string input;
 
+	while(true){
+		std::cout<<"\nPrint 'top' to get top5 titles.\nOr print 'match' to start matching titles: ";
+		std::cin>>input;
+		if(input == "top")
+			printTop5();
+		else if(input == "match")
+			match();
 	}
 }
