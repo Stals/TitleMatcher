f07c8b74db46abbde739ce0561a33d415d499c19
Stanislav Korotaev <superstals@gmail.com>
2012-05-03 00:25:51 +0400

    adds some code to example.cpp and also fixes #3

--- example.cpp
+++ example.cpp
@@ -7,10 +7,10 @@
 #include <iostream>
 #include <string>
 #include "titlematcher.h"
-
+#include "title.h"
 
 void printTop5(TitleMatcher& titleMatcher){
-	std::list<std::string> lst = titleMatcher.getTitles(5);
+	std::list<std::string> lst = titleMatcher.getTitles(100);
 	int i = 1;
 	std::cout<<"Top 5 games:"<<std::endl;
 	for (std::list<std::string>::iterator it = lst.begin(); it!= lst.end(); ++it, ++i){
@@ -19,13 +19,28 @@ void printTop5(TitleMatcher& titleMatcher){
 	std::cout<<std::endl;
 }
 
+void print(int num, Title& title){
+	std::cout<<"#"<<num<<" ";
+	std::cout<<title.name<<std::endl;
+	std::cout<<title.date<<std::endl;
+	std::cout<<title.genre<<std::endl;
+	std::cout<<title.description<<std::endl;
+}
+
 int main(){
 	TitleMatcher titleMatcher;
-	titleMatcher.submitChoice(1);
-	titleMatcher.submitChoice(1);
-	titleMatcher.submitChoice(1);
-	titleMatcher.submitChoice(1);
-	titleMatcher.submitChoice(1);
-	printTop5(titleMatcher);
+	while(true){
+		print(1, titleMatcher.title_1);
+		std::cout<<std::endl;
+		print(2, titleMatcher.title_2);
+
+		std::cout<<"What is your choice? (1 or 2):";
+		std::string input;
+		std::cin>>input;
+		int choice = (input == "1")? 1 : 2;
+
+		titleMatcher.submitChoice(choice);
+		printTop5(titleMatcher);
+	}
 }
 
