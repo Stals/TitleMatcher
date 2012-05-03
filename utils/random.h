613fdc1278fa198bb9a2eaa2f1b4b7c0435785cb
Stanislav Korotaev <superstals@gmail.com>
2012-05-03 19:14:44 +0400

    todo added

--- utils/random.h
+++ utils/random.h
@@ -1,8 +1,7 @@
 #ifndef GETRANDOM_H
 #define GETRANDOM_H
 #include <ctime>
-
-// header to get random number so that i can change algorithm but code depending on it will now change
+// TODO use boost.random
 namespace utils {
 	// returns a random number between l and h
 	int getRandom(unsigned int l, unsigned int h){
