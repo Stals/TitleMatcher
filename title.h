01611fd7d888f4b690b941a9dec76ed1d88bb2e2
Stanislav Korotaev <superstals@gmail.com>
2012-05-03 15:20:38 +0400

    changed string date to int year

--- title.h
+++ title.h
@@ -4,16 +4,16 @@
 struct Title{
 	int id;
 	std::string name;
-	std::string date;
+	int year; //TODO mb make month and day?
 	std::string genre; //TODO could be several genres and i should be able to make matcher for specific genres
 	std::string description;
-	Title():id(0), name(""), date(""), genre(""), description(""){		
+	Title():id(0), name(""), year(0), genre(""), description(""){		
 	}
 
-	Title(int id, std::string name, std::string date, std::string genre, std::string description){
+	Title(int id, std::string name, int year, std::string genre, std::string description){
 		this->id = id;
 		this->name = name;
-		this->date = date;
+		this->year = year;
 		this->genre = genre;
 		this->description = description;
 	}
